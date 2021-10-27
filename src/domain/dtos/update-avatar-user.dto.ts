59fd8b188481cdabd3bed2f7dac8606b9ae3e0fc
William <william.gravina@gcbinvestimentos.com>
2021-10-26 23:30:02 -0300

    :sparkles: feat: created dto for update avatar user

---
+++ src/domain/dtos/update-avatar-user.dto.ts
@@ -0,0 +1,10 @@
+export type IParamsUpdateAvatarUserDTO = {
+  userId: string;
+  avatar: string;
+};
+
+export type IParamsUpdateUserUseCaseDTO = {
+  userId: string;
+  avatar: string;
+  name: string;
+};
