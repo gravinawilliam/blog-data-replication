0d57df6677debc23aae407e04f3df609c5b25bd1
William <william.gravina@gcbinvestimentos.com>
2021-10-26 23:27:18 -0300

    :sparkles: feat: created express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Request, Response } from 'express';
+
+import { IController } from '@shared/interfaces/controller.interface';
+import { IHttpRequest } from '@shared/interfaces/http-request.interface';
+import { HttpStatusCode } from '@shared/utils/http-status-code';
+
+export const adapterRoute = (controller: IController) => {
+  return async (request: Request, response: Response) => {
+    const httpRequest: IHttpRequest = {
+      body: request.body,
+      params: request.params,
+      headers: request.headers,
+    };
+
+    const { body, statusCode } = await controller.handle(httpRequest);
+    if (statusCode >= HttpStatusCode.OK && statusCode <= 299) {
+      response.status(statusCode).json(body);
+    } else {
+      response.status(statusCode).json({
+        error: body.message,
+      });
+    }
+  };
+};
